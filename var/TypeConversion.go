e5ddf025e8322cacae70b6d3c125c2a4be0dd38c
logan.sdk <logan.sdk@kakaoenterprise.com>
2022-09-21 23:28:21 +0900

    Feat: 타입 변경 추가

---
+++ var/TypeConversion.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func main() {
+	strInt := "100"
+	i, err := strconv.Atoi(strInt)
+	fmt.Println(i, err, reflect.TypeOf(i))
+
+	i8, err := strconv.ParseInt(strInt, 0, 8)
+	i16, err := strconv.ParseInt(strInt, 0, 16)
+	i32, err := strconv.ParseInt(strInt, 0, 32)
+	i64, err := strconv.ParseInt(strInt, 0, 64)
+
+	fmt.Println(i8, err, reflect.TypeOf(i8))
+	fmt.Println(i16, err, reflect.TypeOf(i16))
+	fmt.Println(i32, err, reflect.TypeOf(i32))
+	fmt.Println(i64, err, reflect.TypeOf(i64))
+}
