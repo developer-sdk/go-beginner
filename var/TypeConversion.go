22fd443b0f3c7ca055b2e3285662ce2f984e4913
hs_seo <fluorite118@gmail.com>
2022-11-12 09:25:00 +0900

    Fix: 타입 컨버전 처리 추가

--- var/TypeConversion.go
+++ var/TypeConversion.go
@@ -8,18 +8,20 @@ import (
 
 func main() {
 	strInt := "100"
-	fmt.Println(string(strInt))
+	intStr := string(strInt)
+	fmt.Println(intStr, reflect.TypeOf(intStr)) // 100 string
 
 	i, err := strconv.Atoi(strInt)
-	fmt.Println(i, err, reflect.TypeOf(i))
+	fmt.Println(i, err, reflect.TypeOf(i)) //100 <nil> int
 
+	strInt = "987654321"
 	i8, err := strconv.ParseInt(strInt, 0, 8)
 	i16, err := strconv.ParseInt(strInt, 0, 16)
 	i32, err := strconv.ParseInt(strInt, 0, 32)
-	i64, err := strconv.ParseInt(strInt, 0, 64)
+	i64, err := strconv.ParseInt(strInt, 16, 64)
 
-	fmt.Println(i8, err, reflect.TypeOf(i8))
-	fmt.Println(i16, err, reflect.TypeOf(i16))
-	fmt.Println(i32, err, reflect.TypeOf(i32))
-	fmt.Println(i64, err, reflect.TypeOf(i64))
+	fmt.Println(i8, err, reflect.TypeOf(i8))   // 127 <nil> int64
+	fmt.Println(i16, err, reflect.TypeOf(i16)) // 32767 <nil> int64
+	fmt.Println(i32, err, reflect.TypeOf(i32)) // 987654321 <nil> int64
+	fmt.Println(i64, err, reflect.TypeOf(i64)) // 40926266145 <nil> int64
 }
